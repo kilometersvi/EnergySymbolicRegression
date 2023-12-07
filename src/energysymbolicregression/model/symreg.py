a6b2759fdab24c1742b7998d66c7e42d52329525
kilometersvi <milesmilosevich@gmail.com>
2023-12-07 14:55:37 -0800

    tweak

--- src/energysymbolicregression/model/symreg.py
+++ src/energysymbolicregression/model/symreg.py
@@ -239,8 +239,8 @@ class H_SymReg:
             print(i)
             
             if i >= 1:
-                x_data = np.arange(2, i + 1)
-                y_data = self.ghn.E_hist[2: i + 1]
+                x_data = np.arange(1, i + 1)
+                y_data = self.ghn.E_hist[1: i + 1]
 
                 # Check if data arrays are not empty and have the same length
                 if len(x_data) > 0 and len(y_data) > 0 and len(x_data) == len(y_data):
