d45f1f3aefc3d6651b26c8ec8f44ee4d0e8aa076
kilometersvi <milesmilosevich@gmail.com>
2023-12-07 14:57:16 -0800

    tweak

--- src/energysymbolicregression/model/symreg.py
+++ src/energysymbolicregression/model/symreg.py
@@ -220,12 +220,6 @@ class H_SymReg:
         def animate(i):
 
 
-            print(len(self.ghn.E_hist))
-            print(len(c_QV_hist))
-            print(self.ghn.E_hist)
-            print(self.ghn.E_hist[0])
-
-
             s = self.decode_output(V=c_V_hist[i].reshape((self.nUnits,1)))
             y = self.evaluate(s)
             if y is None: y = "err"
@@ -236,7 +230,6 @@ class H_SymReg:
             im_u.set_array(c_u_hist[i])
             im_L.set_array(c_L_hist[i])
             im_QV.set_array(c_QV_hist[i])
-            print(i)
             
             if i >= 1:
                 x_data = np.arange(1, i + 1)
@@ -244,8 +237,6 @@ class H_SymReg:
 
                 # Check if data arrays are not empty and have the same length
                 if len(x_data) > 0 and len(y_data) > 0 and len(x_data) == len(y_data):
-                    print(i)
-                    print('success')
                     line.set_data(x_data, y_data)
                     #line.set_data(np.arange(1, i + 1), self.E_hist[1: i + 1])
                     axes[0, 2].relim()
